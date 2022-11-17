9afa189205341d7506ad09c9db6f0d6c270c0d7f
Daniel <studiosdopplermusic@gmail.com>
2022-11-16 22:42:48 -0500

    data  graph

--- 01_client/src/app/graph/graph.component.ts
+++ 01_client/src/app/graph/graph.component.ts
@@ -7,13 +7,58 @@ import { SocketService } from '../services/socket.service';
   styleUrls: ['./graph.component.scss'],
 })
 export class GraphComponent implements OnInit {
-  constructor(public socketIO: SocketService) {}
-
   data!: number;
+  multi!: any[];
+  legend: boolean = false;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = false;
+  showXAxisLabel: boolean = false;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+  colorScheme: any = {
+    domain: ['#72efdd', '#ffd60a'],
+  };
+
+  constructor(public socketIO: SocketService) {
+    this.multi = [
+      {
+        name: 'BTC',
+        series: [
+          {
+            name: '200',
+            value: 25000,
+          },
+          {
+            name: '201',
+            value: 32000,
+          },
+        ],
+      },
+      {
+        name: 'ETH',
+        series: [],
+      },
+    ];
+  }
 
   ngOnInit(): void {
     this.socketIO.io.on('data_graph', (res) => {
       this.data = res.data;
     });
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
