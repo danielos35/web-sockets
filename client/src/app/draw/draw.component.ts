4d0c6ae0af30285176b763975eeb6b90ef8a2d0d
Daniel <studiosdopplermusic@gmail.com>
2022-11-19 22:53:10 -0500

    canvas

--- client/src/app/draw/draw.component.ts
+++ client/src/app/draw/draw.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-draw',
   templateUrl: './draw.component.html',
-  styleUrls: ['./draw.component.scss']
+  styleUrls: ['./draw.component.scss'],
 })
 export class DrawComponent {
+  @ViewChild('canvasRef', { static: false }) canvasRef: any;
 
+  public width = 800;
+  public height = 800;
+
+  /*
+    Opcion nativa, 
+  */
+  private cd!: CanvasRenderingContext2D;
+  private cordenadas!: Array<any>;
+
+  /*
+    Escuchar evento
+  */
+
+  @HostListener('documnet:mousemove', ['$event'])
+  onMousemove = (evento: any) => {
+    if (evento.target.id === 'canvasId') {
+      console.log(evento);
+    }
+  };
 }
